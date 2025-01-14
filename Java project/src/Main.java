56b8b58e15d6b613c279d5a17e5cea4dee9717e5
controlld <gokhan.temel@live.com>
2025-01-14 14:16:57 +0100

    updated phase 3 with wind list.

--- Java project/src/Main.java
+++ Java project/src/Main.java
@@ -5,17 +5,30 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
+
+    static float[][] windList = {
+            // South and West speeds for 0 m/s -> 1x want, 0 wind.
+            {0, 0},
+            // North      North-East       East     South-East        South     South-West      West       North-West    Speeds for 3 m/s -> 1:N NE E SE S SW W 8:NW
+            {-3, 0}, {-2.121F, -2.121F}, {0, -3}, {2.121F, -2.121F}, {3, 0}, {2.121F, 2.121F}, {0, 3}, {-2.121F, 2.121F},
+            // North      North-East       East     South-East        South     South-West      West       North-West    Speeds for 7 m/s -> 1:N NE E SE S SW W 8:NW
+            {-7, 0}, {-4.950F, -4.950F}, {0, -7}, {4.950F, -4.950F}, {7, 0}, {4.950F, 4.950F}, {0, 7}, {-4.950F, 4.950F},
+            // North      North-East           East       South-East         South      South-West        West       North-West    Speeds for 23 m/s -> 1:N NE E SE S SW W 8:NW
+            {-23, 0}, {-16.263F, -16.263F}, {0, -23}, {16.263F, -16.263F}, {23, 0}, {16.263F, 16.263F}, {0, 23}, {-16.263F, 16.263F}
+    };
+
+
     public static void main(String[] argv) throws IOException {
-        phase2();
-        // phase3();
+        // phase2();
+        phase3();
     }
 
     static void phase2() {
         // In which the forest density is being adjusted between 50 up until 90, in steps of 5.
         for (var i = 1; i <=5; i += 1) {
             String strategy = getStrategy(i);
-            for (var density = 50; density <= 60; density += 5) {
-                startWorkspace(density, 0, 0, strategy, 0);
+            for (var density = 50; density <= 90; density += 5) {
+                startWorkspace(density, 0, strategy, 0);
             }
         }
     }
@@ -24,19 +37,13 @@ public class Main {
         // In which the wind-speed and wind-direction will be added and adjusted, 0/3/7/23 N/W counts 8
         for (var i = 1; i <=5; i += 1) {
             String strategy = getStrategy(i);
-            for (var density=50; density<=60; density+=5) {
-                List<Integer> speedList = Arrays.asList(0, 3, 7 ,23);
-                for (int speed : speedList) {
-                    System.out.println(speed);
-                    for (var dir=1; dir<=8; dir+=1) {  // 1:N NE E SE S SW W 8:NW
-                        var speeds = calculateWind(speed, dir);
-                        startWorkspace(density, (int)speeds[0], (int) speeds[1], strategy, 0);
-                    }
+            for (var density=50; density<=90; density+=5) {
+                for (int z = 0; z < windList.length; z++) {
+                    startWorkspace(density, z, strategy, 0);
                 }
             }
         }
     }
-
 
     static String getStrategy(int i) {
         return switch (i) {
@@ -49,16 +56,7 @@ public class Main {
     }
 
 
-    static float[] calculateWind(int speed, int dir) {
-        // TODO !! Calculate actual wind speed for south, and west, based on desired wind-speed, and direction.
-        float[] speeds = new float[2];
-        speeds[0] = speed;  // SouthSpeed
-        speeds[1] = dir;  // WestSpeed
-        return speeds;
-    }
-
-
-    static void startWorkspace(int forestDensity, int southWindSpeed, int westWindSpeed, String strategy, int ignitionLocation) {
+    static void startWorkspace(int forestDensity, int windListIndex, String strategy, int ignitionLocation) {
         HeadlessWorkspace workspace = HeadlessWorkspace.newInstance();
         try {
             var nlogoFile = Paths.get(System.getProperty("user.dir"), "..", "Simple Fire extension.nlogo");
@@ -66,8 +64,8 @@ public class Main {
             workspace.command("setup");
             workspace.command("set density " + forestDensity);
             workspace.command("set probability-of-spread 70");
-            workspace.command("set south-wind-speed " + southWindSpeed);
-            workspace.command("set west-wind-speed " + westWindSpeed);
+            workspace.command("set south-wind-speed " + windList[windListIndex][0]);
+            workspace.command("set west-wind-speed " + windList[windListIndex][1]);
             workspace.command("set strategy \"" + strategy + "\"");
             workspace.command("random-seed 0");
             workspace.command("run-full");
@@ -76,8 +74,8 @@ public class Main {
 
             System.out.println("Strategy: " + strategy +
                     ", Density: " + forestDensity +
-                    ", SouthWindSpeed: " + southWindSpeed +
-                    ", WestWindSpeed: " + westWindSpeed +
+                    ", SouthWindSpeed: " + windList[windListIndex][0] +
+                    ", WestWindSpeed: " + windList[windListIndex][1] +
                     ", Percent burned: " + percentageBurned +
                     "%, Village damaged: " +villageDamaged);
             workspace.dispose();
